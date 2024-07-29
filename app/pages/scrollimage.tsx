4951e65b869e1333c88546d7cbd1ba41247820d3
Siva S(codingmaster24) <93309902+Sivatech24@users.noreply.github.com>
2024-07-29 11:55:11 +0530

    Update scrollimage.tsx

--- app/pages/scrollimage.tsx
+++ app/pages/scrollimage.tsx
@@ -19,7 +19,7 @@ export function HeroScrollDemo() {
         }
       >
         <Image
-          src={`Images/1.png`}//https://gdurl.com/nJrq
+          src={`Images/1.PNG`}//https://gdurl.com/nJrq
           alt="Moblie Application Development using flutter"
           height={1920}
           width={1080}
